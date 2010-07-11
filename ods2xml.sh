32ea6f9ffe582a7961b65dce93b7179074c3cda0
luzi82 <luzi82@gmail.com>
2010-07-11 18:01:45 +0800

    must put file name as parameter, handle newline

--- ods2xml.sh
+++ ods2xml.sh
@@ -7,5 +7,6 @@ java \
  -cp ${CODETEMPLATE_PATH}/bin/ \
  guri.codetemplate.CodeTemplate \
  -g ${CODETEMPLATE_PATH}/res/code_template_global_config.xml \
- -t ${TEMPLATE_PATH}
+ -t ${TEMPLATE_PATH} \
+ $1
 
